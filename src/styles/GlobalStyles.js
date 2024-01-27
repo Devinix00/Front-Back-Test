69cf31a30b88eaf96cda957d284e09665b1bf39a
Devinix <121074744+Devinix00@users.noreply.github.com>
2024-01-27 18:45:46 +0900

    [CREATE] 스크롤바 디자인 수정

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -37,6 +37,23 @@ const GlobalStyles = createGlobalStyle`
       color: white;
       word-break: keep-all;
     }
+
+    ::-webkit-scrollbar {
+      width: 12px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      border-radius: 10px;
+      background-clip: padding-box;
+      border: 2px solid transparent;
+      background-color: #a7a7a7;
+    }
+
+    ::-webkit-scrollbar-track {
+      background-color: #333d51;
+      border-radius: 10px;
+      box-shadow: inset 0px 0px 5px white;
+    }
 `;
 
 export default GlobalStyles;
